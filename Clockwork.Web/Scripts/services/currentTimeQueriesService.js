9ecf6a0b61fa574ef45c43ffa7b9b6452169f293
Jarrod Swords <jarrod.swords@gmail.com>
2019-06-12 22:21:20 -0400

    Refactor current time queries js service

---
+++ Clockwork.Web/Scripts/services/currentTimeQueriesService.js
@@ -0,0 +1,23 @@
+﻿var Clockwork = Clockwork || {};
+
+Clockwork.CurrentTimeQueriesService = (() => {
+    var getCurrentTime = () => {
+        makeRequest("http://127.0.0.1:5000/api/currenttime", data => {
+            var query = new CurrentTimeQuery(data);
+            app.currentTimeQueries.push(query);
+        })
+    };
+
+    var getCurrentTimeQueries = () => {
+        makeRequest("http://127.0.0.1:5000/api/currenttimequeries", data => {
+            data.forEach(query => {
+                app.currentTimeQueries.push(new CurrentTimeQuery(query));
+            });
+        })
+    };
+
+    return {
+        getCurrentTime,
+        getCurrentTimeQueries
+    };
+})();
