ca65e01d9bf2a2799d46344aeba9c3a978a0dc37
Jarrod Swords <jarrod.swords@gmail.com>
2019-06-12 22:23:56 -0400

    Refactor xhttprequest

---
+++ Clockwork.Web/Scripts/xhttprequest.js
@@ -0,0 +1,12 @@
+﻿var makeRequest = (target, callback = () => { }, httpMethod) => {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var result = JSON.parse(this.responseText);
+            callback(result);
+        }
+    };
+    xhttp.open(httpMethod || "GET", target, true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+};
